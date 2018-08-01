1820a192148cdc3f5ce2ca852e4632275ab19392
Stanislav <stanislav.bereza@gmail.com>
2018-08-01 23:58:43 +0300

    added context to components Composer and Post

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -11,18 +11,11 @@ import Styles from './styles.m.css';
 
 export default class Feed extends Component {
     render () {
-        const { avatar,
-            currentUserFirstName,
-        } = this.props;
-
         return (
             <section className = { Styles.feed }>
-                <StatusBar { ...this.props } />
-                <Composer
-                    avatar = { avatar }
-                    currentUserFirstName = { currentUserFirstName }
-                />
-                <Post { ...this.props } />
+                <StatusBar />
+                <Composer />
+                <Post />
             </section>
         );
     }
